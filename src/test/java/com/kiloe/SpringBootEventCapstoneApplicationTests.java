0fc4bc68ad14d10ff463b36ce4b4339b16073bc1
Myangaa97 <myangaa97@gmail.com>
2026-08-21 17:48:18 +0800

    feat: initialize Spring Boot and PostgreSQL

---
+++ src/test/java/com/kiloe/SpringBootEventCapstoneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiloe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEventCapstoneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
